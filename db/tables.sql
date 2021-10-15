8042130d306173416de80d6d144abb17a1aed5ba
Urmas Rist <urist@noreply.git.01.kood.tech>
2021-10-15 08:53:51 +0000

    SQl create table commands in seperate .sql file (#3)
    Co-authored-by: Urmas Rist <urmas.rist@gmail.com>
    Reviewed-on: https://git.01.kood.tech/Olaroll/forum/pulls/3
    Reviewed-by: Olari Tsernobrovkin <olaritserno@gmail.com>
    Co-authored-by: Urmas Rist <urist@noreply.git.01.kood.tech>
    Co-committed-by: Urmas Rist <urist@noreply.git.01.kood.tech>

---
+++ db/tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `users` (
+		`uid` INTEGER PRIMARY KEY AUTOINCREMENT,
+		`name` TEXT UNIQUE NOT NULL,
+		`email` TEXT UNIQUE NOT NULL,
+		`password` TEXT NOT NULL,
+		`created` DATE
+        );
--- forumDB/fdb.go
+++ forumDB/fdb.go
@@ -2,32 +2,22 @@ package forumDB
 
 import (
 	"database/sql"
+	"os"
 )
 
 // Initialize the database
 func InitializeDB() *sql.DB {
 	// Check if the database doesn't exist
 	db, err := sql.Open("sqlite3", "db/forum.db")
-	checkErr(err)
+	fatalErr(err)
 	CreateTables(db)
 	return db
 }
 
 // Create the initial tables
 func CreateTables(db *sql.DB) {
-	CreateTable(db, "CREATE TABLE IF NOT EXISTS `users` ("+
-		"`uid` INTEGER PRIMARY KEY AUTOINCREMENT,"+
-		"`name` TEXT UNIQUE NOT NULL,"+
-		"`email` TEXT UNIQUE NOT NULL,"+
-		"`password` TEXT NOT NULL,"+
-		"`created` DATE);")
-}
-
-// Create a new table. The string must be a valid sql command, otherwise it will fail.
-func CreateTable(db *sql.DB, s string) {
-	stmt, err := db.Prepare(s)
-	checkErr(err)
-
-	_, err = stmt.Exec()
-	checkErr(err)
+	sqlfile, err := os.ReadFile("db/tables.sql")
+	fatalErr(err)
+	commands := string(sqlfile)
+	db.Exec(commands)
 }

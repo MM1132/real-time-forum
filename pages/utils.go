b3a40e7ca469ee9ce183999f4570ffe8d6a48a06
Urmas Rist <urmas.rist@gmail.com>
2021-10-22 13:29:11 +0300

    Add comments

--- pages/utils.go
+++ pages/utils.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+// Sends an error on the writer with default text
 func sendErr(err error, w http.ResponseWriter, code int) {
 	log.Println(err)
 	http.Error(w, http.StatusText(code), code)
 }
 
+// Recover from panicking goroutine if it's a handler.
 func recoverHandler(w http.ResponseWriter) {
 	if err := recover(); err != nil {
 		http.Error(w, http.StatusText(500), 500)

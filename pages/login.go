fe04c01661ff75ee0084625833620d0ecbbad713
Olari Tsernobrovkin <olaritserno@gmail.com>
2021-10-23 22:56:37 +0300

    Update existing pages with new handler system

--- pages/login.go
+++ pages/login.go
@@ -1,48 +1,27 @@
 package pages
 
 import (
-	"database/sql"
 	"fmt"
-	"forum/forumDB"
-	"forum/utils"
-	"html/template"
+	"forum/forumEnv"
 	"log"
 	"net/http"
 )
 
-// A handler for index.html, containing things that are shared between requests.
-// It should be initialized with IndexHandler()
-type Login struct {
-	db   *sql.DB
-	tmpl *template.Template
-}
+type Login forumEnv.Env
 
 // Contains things that are generated per request
 type loginData struct {
 	Title string // Title should be on every page
 }
 
-// Initializes a page handler with db and template values, and returns a ready to use http.Handler
-func LoginHandler(db *sql.DB, templates map[string]*template.Template) http.Handler {
-	name := "login"
+func (e Login) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tmpl := e.Templates["login"]
 
-	// Get the right template
-	tmpl, ok := templates[name]
-	if !ok {
-		utils.FatalErr(noTemplateError{name})
-	}
-
-	// Define a new Login handler with the db and template set
-	handler := &Login{db: db, tmpl: tmpl}
-	return handler
-}
-
-func (p Login) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// We must create a new loginData struct because it can't be shared between requests
 	data := &loginData{Title: "Forum Login"}
 
 	// Finally execute the template with the data we got
-	if err := p.tmpl.ExecuteTemplate(w, "layout", data); err != nil {
+	if err := tmpl.ExecuteTemplate(w, "layout", data); err != nil {
 		sendErr(err, w, http.StatusInternalServerError)
 		return
 	}
@@ -52,17 +31,17 @@ func (p Login) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 	//
 	case "POST":
-		login(p.db, w, r)
+		e.login(w, r)
 
 	}
 }
 
-func login(db *sql.DB, w http.ResponseWriter, r *http.Request) bool {
+func (e Login) login(w http.ResponseWriter, r *http.Request) bool {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return false
 	}
-	user, err := forumDB.GetUserByName(db, r.FormValue("name"))
+	user, err := e.Users.ByName(r.FormValue("name"))
 	if err != nil {
 		log.Print(err)
 	}
--- pages/register.go
+++ pages/register.go
@@ -1,21 +1,14 @@
 package pages
 
 import (
-	"database/sql"
 	"fmt"
 	"forum/forumDB"
-	"forum/utils"
-	"html/template"
+	"forum/forumEnv"
 	"log"
 	"net/http"
 )
 
-// A handler for index.html, containing things that are shared between requests.
-// It should be initialized with IndexHandler()
-type Register struct {
-	db   *sql.DB
-	tmpl *template.Template
-}
+type Register forumEnv.Env
 
 // Contains things that are generated per request
 type registerData struct {
@@ -23,32 +16,14 @@ type registerData struct {
 	UserCount int
 }
 
-// Initializes a page handler with db and template values, and returns a ready to use http.Handler
-func RegisterHandler(db *sql.DB, templates map[string]*template.Template) http.Handler {
-	name := "register"
+func (e Register) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tmpl := e.Templates["Register"]
 
-	// Get the right template
-	tmpl, ok := templates[name]
-	if !ok {
-		utils.FatalErr(noTemplateError{name})
-	}
-
-	// Define a new Index handler with the db and template set
-	handler := &Register{db: db, tmpl: tmpl}
-	return handler
-}
-
-func (p Register) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// We must create a new indexData struct because it can't be shared between requests
 	data := &registerData{Title: "Forum Register"}
-	// Fill the new data struct with a value we'll use in the template
-	if err := data.getUserRowCount(p.db); err != nil {
-		sendErr(err, w, http.StatusInternalServerError)
-		return
-	}
 
 	// Finally execute the template with the data we got
-	if err := p.tmpl.ExecuteTemplate(w, "layout", data); err != nil {
+	if err := tmpl.ExecuteTemplate(w, "layout", data); err != nil {
 		sendErr(err, w, http.StatusInternalServerError)
 		return
 	}
@@ -58,31 +33,19 @@ func (p Register) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 	//
 	case "POST":
-		register(p.db, w, r)
+		e.register(w, r)
 
 	}
 }
 
-func (d *registerData) getUserRowCount(db *sql.DB) error {
-	row := db.QueryRow("SELECT COUNT(*) FROM users")
-
-	var count int
-	err := row.Scan(&count)
-	if err != nil {
-		return err
-	}
-	d.UserCount = count
-	return nil
-}
-
-func register(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+func (e Register) register(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
 	newUser := forumDB.User{Name: r.FormValue("username"), Email: r.FormValue("email"), Password: r.FormValue("password")}
 
-	_, err := forumDB.InsertUser(db, &newUser)
+	_, err := e.Users.Insert(newUser)
 	if err != nil {
 		log.Println(err)
 	}

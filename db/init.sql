b68a8892f8ae1275ced519b7fc40b27131dd1a4d
Olari Tsernobrovkin <olaritserno@gmail.com>
2021-10-23 22:52:59 +0300

    Added root category

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,8 @@
-INSERT INTO categories (name, description) VALUES ('Main Forum', 'The primary place');
-INSERT INTO categories (name, description) VALUES ('Off-Topic', 'The secondary place');
+INSERT INTO categories (categoryID, name)
+VALUES (0, 'Forum');
+
+INSERT INTO categories (parentID, name, description)
+VALUES (0, 'Main Forum', 'The primary place');
+
+INSERT INTO categories (parentID, name, description)
+VALUES (0, 'Off-Topic', 'The secondary place');

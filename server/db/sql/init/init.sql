00c9d5a73a7166fd218b08f58e9a673d58f0e729
Olari Tsernobrovkin <olaritserno@gmail.com>
2021-11-29 16:15:30 +0200

    asdf

--- server/db/sql/init/init.sql
+++ server/db/sql/init/init.sql
@@ -2,7 +2,7 @@ INSERT INTO boards (boardID, name)
 VALUES (0, 'Forum');
 
 INSERT INTO boards (parentID, name, description, "order")
-VALUES (0, 'Main Forum', 'The primary place', 2);
+VALUES (0, 'Main Forum', 'The primary place', 1);
 
 INSERT INTO boards (parentID, name, description, "order")
-VALUES (0, 'Off-Topic', 'The secondary place', 1);
+VALUES (0, 'Off-Topic', 'The secondary place', 2);
